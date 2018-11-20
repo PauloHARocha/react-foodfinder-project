21aaa430111bbe520c813f84cab906c35b5ed76d
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-11-20 04:03:07 -0300

    Updates

---
+++ precache-manifest.919c1e183952df2059674098b347542a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "294896f769bcf5567190",
+    "url": "/react-foodfinder-project/static/js/runtime~main.294896f7.js"
+  },
+  {
+    "revision": "2d32e62881451a7c8fa0",
+    "url": "/react-foodfinder-project/static/js/main.2d32e628.chunk.js"
+  },
+  {
+    "revision": "6ea4476df7c52da542d9",
+    "url": "/react-foodfinder-project/static/js/1.6ea4476d.chunk.js"
+  },
+  {
+    "revision": "2d32e62881451a7c8fa0",
+    "url": "/react-foodfinder-project/static/css/main.9a4bb441.chunk.css"
+  },
+  {
+    "revision": "b78dea15b59e072f57a39eb553879c8c",
+    "url": "/react-foodfinder-project/index.html"
+  }
+];

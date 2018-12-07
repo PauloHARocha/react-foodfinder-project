e080149c8da62ae736b3bffc9cc9fd131340103b
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-12-07 16:47:36 -0300

    Adding aria labels and roles

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -6,19 +6,26 @@ class SideMenu extends Component {
         return (
             <aside className={`side-menu ${menu ? "visible" : ""}`}>
                 <h2 className="menu-title">Food Finder</h2>
-                <input className="food-finder"
-                placeholder="Search for food here"
-                type="text" onChange={onChangeInput} tabIndex="2"></input>
+                <input
+                    tabIndex="2"
+                    aria-label="search for food" 
+                    className="food-finder"
+                    placeholder="Search for food here"
+                    type="text" 
+                    onChange={onChangeInput} ></input>
                 
                 <ul className="food-list">
                     {showingPlaces.map((place, id) => (
                         <li
                             tabIndex="3"
+                            aria-label="choose restaurant"
+                            role="button"
                             className="food-item"
                             key={place.id}
                             id={id}
                             onKeyPress={createInfoWindowFromList}
-                            onClick={createInfoWindowFromList}>{place.name}</li>
+                            onClick={createInfoWindowFromList}
+                            >{place.name}</li>
                     ))}
                 </ul>
                 
